f3a3e1cded464081dc9eaf3d7671fa099b7ed9b9
alexandre110282 <69106721+alexandre110282@users.noreply.github.com>
2024-04-26 11:49:03 +0200

    Corrections img

--- front-end/src/Components/Nav.jsx
+++ front-end/src/Components/Nav.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import logo from "../Assets/img/argentBankLogo.webp";
+import logo from "../Assets/argentBankLogo.webp";
 import { logout } from '../Redux/reducers/authSlice';
 import { Link, useNavigate } from "react-router-dom";
 
