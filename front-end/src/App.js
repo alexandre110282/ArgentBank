57fe6712ed9154e74f8bf48311de48a71d13f071
alexandre110282 <69106721+alexandre110282@users.noreply.github.com>
2024-05-09 14:28:23 +0200

    Last Commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/Pages/Home.jsx";
 import Sign from "../src/Pages/Sign.jsx";
-import Error from "../src/Pages/Error.jsx";
 import Footer from "./Components/Footer.jsx";
 import Nav from "./Components/Nav.jsx";
 import Users from "./Pages/Users.jsx";
@@ -28,8 +27,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign" element={<Sign />} />
-        <Route path="/*" element={<Error />} />
-        <Route path="/users" element={token ? <Users /> : <Error />} />
+        <Route path="/users" element={token ? <Users /> : <Sign />} />
       </Routes>
       <Footer />
     </Router>
